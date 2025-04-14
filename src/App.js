04bf3a3b8c100519419208b91ae6b8aa69897cc1
Mawuliah Mansaray <mawuliahlmansaray@gmail.com>
2025-04-14 09:01:14 +0100

    adding small content of the welcome

--- src/App.js
+++ src/App.js
@@ -5,7 +5,10 @@ function App() {
   return (
     
     <div>
-      <Typography variant='h2'>Helooo</Typography>
+      <Typography variant='h2'>Welcome to AgricAlert</Typography>
+      <Typography variant='body1'>Your one-stop solution for agricultural alerts.</Typography>
+      <Typography variant='body1'>Stay updated with the latest news and alerts.</Typography>
+      <Typography variant='body1'>Join us in making agriculture smarter!</Typography>
     </div>
   );
 }
