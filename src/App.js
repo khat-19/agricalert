75e95e4bcf406f63825c96e6c7dc5108c125947b
Mawuliah Mansaray <mawuliahlmansaray@gmail.com>
2025-04-14 08:56:18 +0100

    agricalert first commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import { Typography } from '@mui/material';
+import React from 'react';
+
+function App() {
+  return (
+    
+    <div>
+      <Typography variant='h2'>Helooo</Typography>
+    </div>
+  );
+}
+
+export default App;
